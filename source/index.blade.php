18cc4975d69d18c929003fae6c69c1d3648d123a
mithicher <mithnehu@gmail.com>
2020-08-18 17:41:12 +0530

    hero image updated

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,12 +3,12 @@
 @section('body')
 <!-- container max-w-6xl mx-auto  -->
 <div class="bg-gray-100">
-    <section class="py-10 md:py-12 container md:mx-auto px-6 md:px-0">
-        <div class="flex flex-col-reverse mb-10 lg:flex-row lg:mb-24">
-            <div class="flex-1 mt-8 md:pr-16">
+    <section class="pt-6 md:pt-12 container md:mx-auto px-6 md:px-0">
+        <div class="flex flex-col md:flex-row overflow-hidden">
+            <div class="flex-1 mb-10 md:mb-0 md:mt-8 md:pr-16">
                 <h1 id="intro-docs-template" class="tracking-tight">{{ $page->siteName }}</h1>
                 <p class="text-xl mt-2 text-gray-600">{{ $page->siteDescription }}. It includes:</p>
-                <p class="text-lg text-gray-600 mb-8">
+                <p class="text-lg text-gray-600 mb-8 hidden md:block">
                     - Forms <br>    
                     - Datepicker <br>
                     - Drag-n-Drop File Upload <br>
@@ -25,14 +25,14 @@
                 </div>
             </div>
             
-            <div class="w-3/5 hidden md:block">
+            <div class="w-full md:w-3/5">
                 <img 
                     src="/assets/img/hero.svg" 
                     alt="{{ $page->siteName }}" 
-                    class="object-cover">    
+                    class="object-cover -mb-6">    
             </div>
 
-            <img src="/assets/img/hero.svg" alt="{{ $page->siteName }}" class="w-full md:hidden mx-auto mb-8 lg:mb-0">
+           <!--  <img src="/assets/img/hero.svg" alt="{{ $page->siteName }}" class="w-full md:hidden mx-auto mb-8 lg:mb-0"> -->
         </div>
     </section>
 </div>
